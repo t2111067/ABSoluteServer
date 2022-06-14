35aa6408849cfe9223883cf9d4ecefd7e867010b
이춘우[모바일센터] <47101009+t2111067@users.noreply.github.com>
2022-06-14 11:00:22 +0900

    Initial commit

---
+++ src/main/java/com/abs/absoluteserver/AbSoluteServerApplication.java
@@ -0,0 +1,13 @@
+package com.abs.absoluteserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbSoluteServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbSoluteServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abs/absoluteserver/AbSoluteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abs.absoluteserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbSoluteServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
